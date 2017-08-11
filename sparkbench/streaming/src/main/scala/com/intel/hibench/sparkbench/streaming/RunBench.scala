068a7a5da911ae11927353fdca66ef734a0c041b
Baptiste MICHAUD <root@otnddc-had-amb01.itt.den.denroot.astrium.corp>
2017-08-11 10:08:22 +0200

    Disable RunBench for first release

--- sparkbench/streaming/src/main/scala/com/intel/hibench/sparkbench/streaming/RunBench.scala
+++ sparkbench/streaming/src/main/scala/com/intel/hibench/sparkbench/streaming/RunBench.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/*
 package com.intel.hibench.sparkbench.streaming
 
 import com.intel.hibench.common.HiBenchConfig
@@ -118,3 +118,4 @@ object RunBench {
   }
 
 }
+*/
